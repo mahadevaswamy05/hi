c9fecc70bf24f8266b8066f9216eef51ad2fa3c8
mahadevaswamy05 <mahadevaswamy@mirafra.com>
2024-11-16 20:12:43 +0530

    updated the parameter code file

--- practice_code/parmeter_within_macros.sv
+++ practice_code/parmeter_within_macros.sv
@@ -1,15 +1,14 @@
-
 parameter PCIE_MAP_START = 32'h3000_1000;
 parameter PCIE_MAP_END = 32'h3000_4000;
 `define TB_MEMORY_MAP_PCIE_START PCIE_MAP_START
 `define TB_MEMORY_MAP_PCIE_END PCIE_MAP_END
 
 module test;
-int check_value;
+int a = 32'h3000_2000;
 initial begin
-  if(check_value >= `TB_MEMORY_MAP_PCIE_START &&
-    check_value < `TB_MEMORY_MAP_PCIE_END) begin
-    $display("Within the range");
+  if(a >= `TB_MEMORY_MAP_PCIE_START &&
+    a < `TB_MEMORY_MAP_PCIE_END) begin
+    $display("a variable value is Within the Parameter range");
   end
 end
 endmodule
